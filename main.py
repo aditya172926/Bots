1aad5a5c3fb941f591e0594701cc0bfb1d6184f2
Aditya <aditya26sg@gmail.com>
2021-05-15 01:45:23 +0530

    getting the names

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from time import sleep
+import os
 
 driver = webdriver.Chrome()
 driver.maximize_window()
@@ -24,6 +25,49 @@ class InstaBot:
     def get_unfollowers(self):
         driver.find_element_by_xpath('//a[contains(text(), "aditya_1729sg")]').click()
         sleep(4)
+        driver.find_element_by_xpath('//a[contains(@href, "/following")]').click()
+        sleep(1)
+        try:
+            sugs = driver.find_element_by_xpath('//h4[contains(text(), Suggessions)]')
+            driver.execute_script('arguements[0].scrollIntoView()', sugs)
+            sleep(1)
+        except:
+            print('No suggessions')
+            pass
+        print('Scroll to the bottom')
+        sleep(2) 
+        done = input("Are you done?")
+        following = self._get_names()
+
+        driver.find_element_by_xpath('//a[contains(@href, "/followers")]').click()
+        sleep(1)
+        try:
+            sugs = driver.find_element_by_xpath('//h4[contains(text(), Suggessions)]')
+            driver.execute_script('arguements[0].scrollIntoView()', sugs)
+            sleep(1)
+        except:
+            print('No suggessions')
+            pass
+        print('Scroll to the bottom')
+        sleep(2) 
+        done = input("Are you done?")
+        followers = self._get_names()
+
+        not_following_back = [user for user in following if user not in followers]
+        print(not_following_back)
+
+    def _get_names(self):
+        links = driver.find_elements_by_tag_name('a')
+        names = [name.text for name in links if name.text != '']
+        print(names)
+        driver.find_element_by_xpath('/html/body/div[5]/div/div/div[1]/div/div[2]/button/div').click()
+        return names
+
 
 
 mybot = InstaBot('aditya26sg@gmail.com', 'Shivbaba26$')
+
+
+# To run an interactive shell use the command 'python3 -i main.py' then you can 
+# type 'mybot.get_unfollowers()' anytime in the program and till then the selenium
+# will wait
