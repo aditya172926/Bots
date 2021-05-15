c8ffecad90f8538c69b286ea3c3fdbe34f2c53b8
Aditya <aditya26sg@gmail.com>
2021-05-15 17:02:54 +0530

    flask integrated with the main module

--- home.py
+++ home.py
@@ -1,7 +1,30 @@
 from  flask import Flask, render_template
+import main
+from time import sleep
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    return render_template('home.html')
+
+@app.route('/getnames/', methods=['POST'])
+def getTheNames():
+    testing.bot.get_followings()
+    sleep(2)
+    done = input("Are you done?")
+    following = testing.bot._get_names()
+    sleep(1)
+    testing.bot.get_followers()
+    sleep(2)
+    done = input("Are you done?")
+    followers = testing.bot._get_names()
+
+    not_following_back = [user for user in following if user not in followers]
+    print(not_following_back)
+    return render_template('home.html', notList = not_following_back)
+
+@app.route('/instabot/', methods=['POST'])
+def testing():
+    testing.bot = main.InstaBot('aditya26sg@gmail.com', 'Shivbaba26$')
     return render_template('home.html')
--- main.py
+++ main.py
@@ -2,70 +2,66 @@ from selenium import webdriver
 from time import sleep
 import os
 
-driver = webdriver.Chrome()
-driver.maximize_window()
-
 class InstaBot:
     def __init__ (self, username, pw):
         self.username = username
-
-        driver.get('https://www.instagram.com/')
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get('https://www.instagram.com/')
         sleep(2)
-        driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
-        driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(pw)
-        driver.find_element_by_xpath('//button[@type="submit"]').click()
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(pw)
+        self.driver.find_element_by_xpath('//button[@type="submit"]').click()
         sleep(4)
         try:
-            driver.find_element_by_xpath('//button[contains(text(), "Not Now")]').click()
+            self.driver.find_element_by_xpath('//button[contains(text(), "Not Now")]').click()
             sleep(4)
-            driver.find_element_by_xpath('//button[contains(text(), "Not Now")]').click()
+            self.driver.find_element_by_xpath('//button[contains(text(), "Not Now")]').click()
         except:
             print('The Save password or Notifications enable popup did not occur did not occur')
+        sleep(2)
+        self.driver.find_element_by_xpath('//a[contains(text(), "aditya_1729sg")]').click()
 
-    def get_unfollowers(self):
-        driver.find_element_by_xpath('//a[contains(text(), "aditya_1729sg")]').click()
-        sleep(4)
-        driver.find_element_by_xpath('//a[contains(@href, "/following")]').click()
+
+    def get_followings(self):
+        sleep(2)
+        self.driver.find_element_by_xpath('//a[contains(@href, "/following")]').click()
         sleep(1)
         try:
-            sugs = driver.find_element_by_xpath('//h4[contains(text(), Suggessions)]')
-            driver.execute_script('arguements[0].scrollIntoView()', sugs)
+            sugs = self.driver.find_element_by_xpath('//h4[contains(text(), Suggessions)]')
+            self.driver.execute_script('arguements[0].scrollIntoView()', sugs)
             sleep(1)
         except:
             print('No suggessions')
             pass
         print('Scroll to the bottom')
         sleep(2) 
-        done = input("Are you done?")
-        following = self._get_names()
+        
 
-        driver.find_element_by_xpath('//a[contains(@href, "/followers")]').click()
+    def get_followers(self):
+        self.driver.find_element_by_xpath('//a[contains(@href, "/followers")]').click()
         sleep(1)
         try:
-            sugs = driver.find_element_by_xpath('//h4[contains(text(), Suggessions)]')
-            driver.execute_script('arguements[0].scrollIntoView()', sugs)
+            sugs = self.driver.find_element_by_xpath('//h4[contains(text(), Suggessions)]')
+            self.driver.execute_script('arguements[0].scrollIntoView()', sugs)
             sleep(1)
         except:
             print('No suggessions')
             pass
         print('Scroll to the bottom')
-        sleep(2) 
-        done = input("Are you done?")
-        followers = self._get_names()
-
-        not_following_back = [user for user in following if user not in followers]
-        print(not_following_back)
+        sleep(2)
 
     def _get_names(self):
-        links = driver.find_elements_by_tag_name('a')
+        links = self.driver.find_elements_by_tag_name('a')
         names = [name.text for name in links if name.text != '']
         print(names)
-        driver.find_element_by_xpath('/html/body/div[5]/div/div/div[1]/div/div[2]/button/div').click()
-        return names
+        listone = names
+        self.driver.find_element_by_xpath('/html/body/div[5]/div/div/div[1]/div/div[2]/button/div').click()
+        return listone
 
 
 
-mybot = InstaBot('aditya26sg@gmail.com', 'Shivbaba26$')
+# mybot = InstaBot('aditya26sg@gmail.com', 'Shivbaba26$')
 
 
 # To run an interactive shell use the command 'python3 -i main.py' then you can 
